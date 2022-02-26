79f9b0292e20f6e04fc7fb5a3cf46cb70bd7610f
Alex <aboyle@zap.me>
2022-02-26 13:33:36 +1300

    Prepare backend for file uploads

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     ''' Returns index template '''
+    if request.method == 'POST':
+        if request.files['image']:
+            return "has files"
     return render_template('index.html')
 
 if __name__ == "__main__":
