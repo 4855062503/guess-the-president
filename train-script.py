6fb88438a064ce39d142c45884ed7d3c8e236df8
Alex <aboyle@zap.me>
2022-02-26 22:49:34 +1300

    Start on face recognition stuff

---
+++ train-script.py
@@ -0,0 +1,63 @@
+import cv2
+import os
+
+def draw_rectangle(img, rect):
+    (x, y, w, h) = rect
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+def draw_text(img, text, x, y):
+    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+
+def detect_face(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier('opencv-files/lbpcascade_frontalface.xml')
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5);
+    if (len(faces) == 0):
+        return None, None
+    (x, y, w, h) = faces[0]
+    return gray[y:y+w, x:x+h], faces[0]
+
+def prepare_training_data(data_folder_path):
+    dirs = os.listdir(data_folder_path)
+    #list to hold all subject faces
+    faces = []
+    #list to hold labels for all subjects
+    labels = []
+
+    #let's go through each directory and read images within it
+    for dir_name in dirs:
+        if not dir_name.startswith("s"):
+            continue;
+
+        label = int(dir_name.replace("s", ""))
+        subject_dir_path = data_folder_path + "/" + dir_name
+        subject_images_names = os.listdir(subject_dir_path)
+        for image_name in subject_images_names:
+            #ignore system files like .DS_Store
+            if image_name.startswith("."):
+                continue;
+
+            image_path = subject_dir_path + "/" + image_name
+            image = cv2.imread(image_path)
+            face, rect = detect_face(image)
+            if face is not None:
+                faces.append(face)
+                labels.append(label)
+
+    return faces, labels
+
+faces, labels = prepare_training_data("faces")
+
+# Instantiate the face_recognizer
+
+face_recognizer = cv2.face.createLBPHFaceRecognizer()
+face_recognizer.train(faces, np.array(labels))
+
+def predict(test_img):
+    img = test_img.copy()
+    face, rect = detect_face(img)
+    label = face_recognizer.predict(face)
+    label_text = subjects[label]
+    draw_rectangle(img, rect)
+    draw_text(img, label_text, rect[0], rect[1]-5)
+    return img
