c20fb07d602042a7e64d4ae56ee0a32058f96453
Alex <aboyle@zap.me>
2022-02-28 13:15:21 +1300

    Remove unnecessary stuff from train-script.py

--- train-script.py
+++ train-script.py
@@ -1,21 +1,10 @@
+'''
+Trains our face-recognition model and saves as an XML file
+'''
+
 from utils import *
 
 faces, labels = prepare_training_data("faces")
-# Instantiate the face_recognizer
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 face_recognizer.train(faces, np.array(labels))
-
-def predict(test_img):
-    subjects = ["", "Joe Biden", "Donald Trump"]
-    img = test_img.copy()
-    face, rect = detect_face(img)
-    label, conf = face_recognizer.predict(face)
-    label_text = subjects[label]
-    print("President predicted is " + label_text)
-    draw_rectangle(img, rect)
-    draw_text(img, label_text, rect[0], rect[1]-5)
-    return img
-
-test_img = cv2.imread('test-img.jpg')
-predicted_img = predict(test_img)
 face_recognizer.save('model.xml')
